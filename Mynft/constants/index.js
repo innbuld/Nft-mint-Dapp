1da1d4aa54b9291a0871ade351cadba92ae313ca
innbuld <kanmiolanrewaju@gmail.com>
2022-07-19 04:07:25 -0700

    first commit

---
+++ Mynft/constants/index.js
@@ -0,0 +1,5 @@
+
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "ipfs://Qma8AJuU3fAGpxvoHzSXoz2WkVbMYkauxuZfA6JnbWDxhC/";
+
+module.exports = {METADATA_URL};
---
+++ Mynft/scripts/deploy.js
@@ -0,0 +1,32 @@
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const {METADATA_URL} = require("../constants");
+
+async function main() {
+  // URL from where we can extract the metadata for a Crypto Dev NFT
+  const metadataURL = METADATA_URL;
+
+/*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so cryptoDevsContract here is a factory for instances of our CryptoDevs contract.
+  */
+  const citaContract = await ethers.getContractFactory("cita");
+
+  // deploy the contract
+  const deployedcitaContract = await citaContract.deploy(
+    metadataURL,
+  
+  );
+  // print the address of the deployed contract
+  console.log(
+    "cita Contract Address:",
+    deployedcitaContract.address
+  );
+}
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
